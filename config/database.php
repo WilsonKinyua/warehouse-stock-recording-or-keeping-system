4b374b346f2242baef07819c8a33d9c3afaaee1a
wilson <wilsonkinyuam@gmail.com>
2020-09-21 22:16:26 +0300

    start of warehouse system

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$user       = "root";
+$host       = "localhost";
+$password   = "";
+$database   = "warehouse";
+
+
+$connection = mysqli_connect($host,$user,$password,$database);
+
+if(!$connection) {
+    die("QUERY FAILED" . mysqli_error($connection));
+}
